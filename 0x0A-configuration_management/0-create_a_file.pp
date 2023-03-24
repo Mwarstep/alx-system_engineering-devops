8904111a2b53df5f957ed562c351e31998ebc876
Mwarstep <mvoi811@gmail.com>
2023-03-24 06:44:17 -0700

    Task 0 - Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# A new file in /tmp will be created
+
+file { '/tmp/school':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
