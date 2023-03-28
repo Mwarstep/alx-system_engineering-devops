8a87473a71716d36a0abcba53ac034f7f4ac4fef
Arnold Mwarstep <111042323+Mwarstep@users.noreply.github.com>
2023-03-28 20:03:51 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -8,14 +8,14 @@ file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
 
-file_line { '/redirect_me 301:
+file_line { '/redirect_me 301':
   ensure => present,
-  after => 'listen 80 default_server;',
-  path => '/etc/nginx/sites-available/default',
-  line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 premanent;',
+  after  => 'listen 80 default_server;',
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
-  ensure => running,
+  ensure  => running,
   require => Package['nginx'],
 }
