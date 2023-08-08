33dd5bb1c60d1f63679349adae1ead0a9e3a9bf0
Mwarstep <mvoi811@gmail.com>
2023-08-08 05:43:22 -0700

    Tests v.2.5

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This prints the titles of the first 10 hot posts listed for a given subreddit
+Will print the titles of the first 10 hot posts listed for a given subreddit
 """
 
 from requests import get
@@ -14,11 +14,10 @@ def top_ten(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
-        return
 
     user_agent = {'User-agent': 'Mozilla/5.0'}
     params = {'limit': 10}
-    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
     results = response.json()
@@ -26,12 +25,8 @@ def top_ten(subreddit):
     try:
         my_data = results.get('data').get('children')
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+        for x in my_data:
+            print(x.get('data').get('title'))
 
     except Exception:
         print("None")
-
-
-subreddit = input("Enter subreddit name: ")
-top_ten(subreddit)
