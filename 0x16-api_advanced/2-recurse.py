c9be5f468a3756393d1be032d9706055ae8db6e5
Mwarstep <mvoi811@gmail.com>
2023-08-08 05:52:25 -0700

    Tests v.2.6

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,10 +3,13 @@
 import requests
 
 
-def get_hot_titles(subreddit, after=None, hot_list=[]):
+def recurse(subreddit, hot_list=None):
     """Will return top ten post titles recursively"""
+    if hot_list is None:
+        hot_list = []
+
     user_agent = {'User-Agent': 'api_advanced-project'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     parameters = {'after': after}
     results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
@@ -14,20 +17,21 @@ def get_hot_titles(subreddit, after=None, hot_list=[]):
     if results.status_code == 200:
         after_data = results.json().get("data").get("after")
         if after_data is not None:
-            return get_hot_titles(subreddit, after_data, hot_list)
+            recurse(subreddit, hot_list)
         all_titles = results.json().get("data").get("children")
-        hot_list.extend(title_.get("data").get("title")
-                        for title_ in all_titles)
+        for title_data in all_titles:
+            hot_list.append(title_data.get("data").get("title"))
         return hot_list
     else:
         return None
 
 
 subreddit = input("Enter subreddit name: ")
-hot_titles = get_hot_titles(subreddit)
+hot_titles = recurse(subreddit)
 
 if hot_titles is not None:
     for index, title in enumerate(hot_titles, start=1):
         print(f"{index}. {title}")
 else:
     print("An error occurred or the subreddit does not exist.")
+
